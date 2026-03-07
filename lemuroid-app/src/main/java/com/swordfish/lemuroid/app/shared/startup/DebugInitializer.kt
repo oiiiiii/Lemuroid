b5fea9b915b10e3d990a5d89aa6abc015569c146
oiiiiii <dassrenvans@gmail.com>
2026-03-07 17:53:22 +0800

    让发行版的程序仅保留 I/W/E 级别的日志。

--- lemuroid-app/src/main/java/com/swordfish/lemuroid/app/shared/startup/DebugInitializer.kt
+++ lemuroid-app/src/main/java/com/swordfish/lemuroid/app/shared/startup/DebugInitializer.kt
@@ -2,6 +2,7 @@ package com.swordfish.lemuroid.app.shared.startup
 
 import android.content.Context
 import android.os.StrictMode
+import android.util.Log
 import androidx.startup.Initializer
 import com.swordfish.lemuroid.BuildConfig
 import timber.log.Timber
@@ -11,6 +12,16 @@ class DebugInitializer : Initializer<Unit> {
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
             enableStrictMode()
+        } else {
+            // 在发行版中仅保留 I/W/E 级别的日志
+            Timber.plant(object : Timber.Tree() {
+                override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+                    // 只记录 INFO、WARN 和 ERROR 级别的日志
+                    if (priority == Log.INFO || priority == Log.WARN || priority == Log.ERROR) {
+                        super.log(priority, tag, message, t)
+                    }
+                }
+            })
         }
     }
 
